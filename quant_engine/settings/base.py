0ae853983eef8aeb434b46384a4cd25e070c0b10
eric <ericisnotrealname@gmail.com>
2026-09-02 14:29:30 +0800

    add arcis middleware

--- quant_engine/settings/base.py
+++ quant_engine/settings/base.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'arcis.django.ArcisMiddleware',
 ]
 
 ROOT_URLCONF = 'quant_engine.urls'
